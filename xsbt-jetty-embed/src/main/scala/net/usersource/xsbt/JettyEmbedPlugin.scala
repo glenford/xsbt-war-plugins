57c0dcbb13e69a73aad6ffb044fca0e3f17584ce
glenford <glen.a.ford@gmail.com>
2011-07-28 14:51:43 +0100

    refactoring, extracted common trait and methods

--- xsbt-jetty-embed/src/main/scala/net/usersource/xsbt/JettyEmbedPlugin.scala
+++ xsbt-jetty-embed/src/main/scala/net/usersource/xsbt/JettyEmbedPlugin.scala
@@ -9,27 +9,13 @@ import sbt.Project.Initialize
 
 import sbt.File
 
-
-object TomcatEmbedPlugin extends Plugin {
-  val TomcatEmbed = config("tomcatEmbed") extend(Runtime)
-
-  val tomcat7DefaultStartupClass = "net.usersource.tomcatembed.tomcat7.Startup"
-
-  val embedTomcatPrepare = TaskKey[Seq[(File,String)]]("embed-tomcat-prepare")
-  val embedTomcat = TaskKey[File]("embed-tomcat", "embed tomcat to produce an executeable war")
-
-  val tomcatVersion = SettingKey[String]("embed-tomcat-version")
-  val tomcatEmbeddedStartup = TaskKey[Option[String]]("embed-tomcat-startup")
-
-  val tomcatEmbeddedClasspath = TaskKey[Seq[ModuleID]]("embed-tomcat-classpath")
-
+trait EmbedPlugin extends Plugin {
+  
   // these are set by the xsbt-web plugin (https://github.com/siasia/xsbt-web-plugin)
   val prepareWar = TaskKey[Seq[(File, String)]]("prepare-webapp")
   val temporaryWarPath = SettingKey[File]("temporary-war-path")
 
-  def embedTomcatTask:  Initialize[Task[Seq[(File, String)]]] = embedTomcatPrepare map { (e) => e }
-
-  private def prepareStartupClass( classDir: File, className: String, log :Logger ): Option[(File,String)] = {
+  def prepareStartupClass( classDir: File, className: String, log :Logger ): Option[(File,String)] = {
     val fileName = className.replace('.','/') + ".class"
     val file = new File(classDir, fileName)
     if(file.exists()) {
@@ -41,7 +27,6 @@ object TomcatEmbedPlugin extends Plugin {
       val pluginJar = new File("project/plugins/lib/xsbt-jetty-embed_2.8.1-0.3.jar")
       log.debug("Using plugin JAR [" + pluginJar.absolutePath + "]")
       val files = IO.unzip(pluginJar, classDir, filter = (s: String) => s.endsWith(fileName + ".precompiled") )
-
       log.debug("Unpacked [" + files.head.getAbsolutePath + "]" )
       IO.move(files.head ,classDir / fileName)
       if(file.exists()) Some((file,fileName))
@@ -49,18 +34,18 @@ object TomcatEmbedPlugin extends Plugin {
     }
   }
 
-  private def handleNoStartup(startup: String, log: Logger) = {
+  def handleNoStartup(startup: String, log: Logger) = {
     log.error("Unable to find the startup class file for [" + startup + "] please check your startup class setting")
     Seq.empty
   }
 
-  private def embedTomcatIntoWarPath( warPath: File, startUpFile: File, relPath: String, className: String, tomcatDeps: Seq[File], log: Logger ) = {
+  def embedIntoWarPath( warPath: File, startUpFile: File, relPath: String, className: String, deps: Seq[File], log: Logger ) = {
     log.debug("Resolves to startup class file [" + startUpFile.absolutePath + "]")
     val dest = warPath / relPath
     IO.copyFile(startUpFile,dest,false)
 
-    log.debug("Embedding tomcat dependencies [" + tomcatDeps + "]")
-    tomcatDeps.foreach { (dep) => {
+    log.debug("Embedding dependencies [" + deps + "]")
+    deps.foreach { (dep) => {
       log.debug("Unpacking [" + dep + "] to [" + warPath / dep.name + "]")
       val filter: NameFilter = (s: String) => !s.endsWith(".MF")
       IO.unzip(dep,warPath,filter)
@@ -70,15 +55,32 @@ object TomcatEmbedPlugin extends Plugin {
     (warPath).descendentsExcept("*", ".svn") x (relativeTo(warPath)|flat)
   }
 
+  def embedPrepare(classDir: File, startup: String, log: AbstractLogger, warPath: File, tomcatDeps: scala.Seq[File]): Seq[(File, String)] = {
+    prepareStartupClass(classDir, startup, log) match {
+      case Some((file: File, relPath: String)) => embedIntoWarPath(warPath, file, relPath, startup, tomcatDeps, log)
+      case None => handleNoStartup(startup, log)
+    }
+  }
+}
+
+
+object TomcatEmbedPlugin extends EmbedPlugin {
+  val TomcatEmbed = config("tomcatEmbed") extend(Runtime)
+  val tomcat7DefaultStartupClass = "net.usersource.tomcatembed.tomcat7.Startup"
+  val embedTomcatPrepare = TaskKey[Seq[(File,String)]]("embed-tomcat-prepare")
+  val embedTomcat = TaskKey[File]("embed-tomcat", "embed tomcat to produce an executeable war")
+  val tomcatVersion = SettingKey[String]("embed-tomcat-version")
+  val tomcatEmbeddedStartup = TaskKey[Option[String]]("embed-tomcat-startup")
+  val tomcatEmbeddedClasspath = TaskKey[Seq[ModuleID]]("embed-tomcat-classpath")
+
+  def embedTomcatTask:  Initialize[Task[Seq[(File, String)]]] = embedTomcatPrepare map { (e) => e }
+
+
+
   def embedTomcatPrepareTask(warPath: File, startup: String, classDir: File, tomcatDeps: Seq[File], slog: Logger): Seq[(File, String)] = {
     val log = slog.asInstanceOf[AbstractLogger]
-
     log.debug("Preparing to embed tomcat into war currently in [" + warPath + "] with [" + startup + "]")
-
-    prepareStartupClass(classDir,startup,log) match {
-      case Some((file: File,relPath: String)) => embedTomcatIntoWarPath(warPath,file,relPath,startup,tomcatDeps,log)
-      case None => handleNoStartup(startup,log)
-    }
+    embedPrepare(classDir, startup, log, warPath, tomcatDeps)
   }
 
   private def determineStartup = {
@@ -102,9 +104,7 @@ object TomcatEmbedPlugin extends Plugin {
     }
   }
 
- override lazy val settings = super.settings ++ tomcatEmbedSettings
-
- lazy val tomcatEmbedSettings: Seq[Project.Setting[_]] = {
+  lazy val tomcatEmbedSettings: Seq[Project.Setting[_]] = {
     Seq(
       tomcatVersion := "7.0.2",
       tomcatEmbeddedStartup <<= determineStartup,
@@ -123,80 +123,28 @@ object TomcatEmbedPlugin extends Plugin {
     ) ++ packageTasks(embedTomcat,embedTomcatTask)
   }
 
+  override lazy val settings = super.settings ++ tomcatEmbedSettings
 }
 
 
-object JettyEmbedPlugin extends Plugin {
+object JettyEmbedPlugin extends EmbedPlugin {
 
   val JettyEmbed = config("jettyEmbed") extend(Runtime)
-
   val jetty6DefaultStartupClass = "net.usersource.jettyembed.jetty6.Startup"
   val jetty7DefaultStartupClass = "net.usersource.jettyembed.jetty7.Startup"
-
   val embedJettyPrepare = TaskKey[Seq[(File,String)]]("embed-jetty-prepare")
   val embedJetty = TaskKey[File]("embed-jetty", "embed jetty to produce an executeable war")
-
   val jettyVersion = SettingKey[String]("embed-jetty-version")
   val jettyEmbeddedStartup = TaskKey[Option[String]]("embed-jetty-startup")
-
   val jettyEmbeddedClasspath = TaskKey[Seq[ModuleID]]("embed-jetty-classpath")
 
-  // these are set by the xsbt-web plugin (https://github.com/siasia/xsbt-web-plugin)
-  val prepareWar = TaskKey[Seq[(File, String)]]("prepare-webapp")
-  val temporaryWarPath = SettingKey[File]("temporary-war-path")
 
   def embedJettyTask:  Initialize[Task[Seq[(File, String)]]] = embedJettyPrepare map { (e) => e }
 
-  private def prepareStartupClass( classDir: File, className: String, log :Logger ): Option[(File,String)] = {
-    val fileName = className.replace('.','/') + ".class"
-    val file = new File(classDir, fileName)
-    if(file.exists()) {
-      log.debug("Found startup class [" + file.getAbsolutePath + "]")
-      Some((file,fileName))
-    }
-    else {
-      log.debug("Startup class not found, unpacking precompiled version")
-      val pluginJar = new File("project/plugins/lib/xsbt-jetty-embed_2.8.1-0.3.jar")
-      log.debug("Using plugin JAR [" + pluginJar.absolutePath + "]")
-      val files = IO.unzip(pluginJar, classDir, filter = (s: String) => s.endsWith(fileName + ".precompiled") )
-      
-      log.debug("Unpacked [" + files.head.getAbsolutePath + "]" )
-      IO.move(files.head ,classDir / fileName)
-      if(file.exists()) Some((file,fileName))
-      else None
-    }
-  }
-
-  private def handleNoStartup(startup: String, log: Logger) = {
-    log.error("Unable to find the startup class file for [" + startup + "] please check your startup class setting")
-    Seq.empty
-  }
-
-  private def embedJettyIntoWarPath( warPath: File, startUpFile: File, relPath: String, className: String, jettyDeps: Seq[File], log: Logger ) = {
-    log.debug("Resolves to startup class file [" + startUpFile.absolutePath + "]")
-    val dest = warPath / relPath
-    IO.copyFile(startUpFile,dest,false)
-
-    log.debug("Embedding jetty dependencies [" + jettyDeps + "]")
-    jettyDeps.foreach { (dep) => {
-      log.debug("Unpacking [" + dep + "] to [" + warPath / dep.name + "]")
-      val filter: NameFilter = (s: String) => !s.endsWith(".MF")
-      IO.unzip(dep,warPath,filter)
-    } }
-
-    // TODO: need a better way to get descendents
-    (warPath).descendentsExcept("*", ".svn") x (relativeTo(warPath)|flat)
-  }
-
   def embedJettyPrepareTask(warPath: File, startup: String, classDir: File, jettyDeps: Seq[File], slog: Logger): Seq[(File, String)] = {
     val log = slog.asInstanceOf[AbstractLogger]
-
     log.debug("Preparing to embed jetty into war currently in [" + warPath + "] with [" + startup + "]")
-
-    prepareStartupClass(classDir,startup,log) match {
-      case Some((file: File,relPath: String)) => embedJettyIntoWarPath(warPath,file,relPath,startup,jettyDeps,log)
-      case None => handleNoStartup(startup,log)
-    }
+    embedPrepare(classDir, startup, log, warPath, jettyDeps)
   }
 
   private def determineStartup = {
@@ -225,9 +173,7 @@ object JettyEmbedPlugin extends Plugin {
     }
   }
 
- override lazy val settings = super.settings ++ jettyEmbedSettings
-
- lazy val jettyEmbedSettings: Seq[Project.Setting[_]] = {
+  lazy val jettyEmbedSettings: Seq[Project.Setting[_]] = {
     Seq(
       jettyVersion := "6.1.21",
       jettyEmbeddedStartup <<= determineStartup,
@@ -246,6 +192,6 @@ object JettyEmbedPlugin extends Plugin {
     ) ++ packageTasks(embedJetty,embedJettyTask)
   }
 
-
+  override lazy val settings = super.settings ++ jettyEmbedSettings
 }
 
